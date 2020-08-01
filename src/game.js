c91b10dad937460c35bd6b853e8adff1a19d8622
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-08-01 15:00:23 -0600

    Update and test game.js methods

--- src/game.js
+++ src/game.js
@@ -1,15 +1,15 @@
 class Game {
   constructor(player1, player2) {
-    this.player1 = player1
-    this.player2 = player2
-    gameboard = [0,0,0,0,0,0,0,0,0]
-    player1turn = true
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = [0,0,0,0,0,0,0,0,0];
+    this.player1turn = true;
   }
-  updateBoard() {
+  updateBoard(sector, player) {
 
   }
   resetBoard() {
-    
+
   }
   passTurn() {
 
