cf71918489f629187d4ec68528439e78d558c092
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-08-01 14:46:47 -0600

    Add possible keys and methods for framework

--- src/game.js
+++ src/game.js
@@ -2,5 +2,19 @@ class Game {
   constructor(player1, player2) {
     this.player1 = player1
     this.player2 = player2
+    gameboard = [0,0,0,0,0,0,0,0,0]
+    player1turn = true
+  }
+  updateBoard() {
+
+  }
+  resetBoard() {
+    
+  }
+  passTurn() {
+
+  }
+  checkForWin() {
+
   }
 }
