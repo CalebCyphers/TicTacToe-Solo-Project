a932a19a8d417c56456f9e4ade398288616cac01
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-08-02 15:39:56 -0600

    add logic for UpdateMoard and passTurn

--- src/game.js
+++ src/game.js
@@ -5,14 +5,24 @@ class Game {
     this.board = [0,0,0,0,0,0,0,0,0];
     this.player1turn = true;
   }
-  updateBoard(sector, player) {
-
+  updateBoard(sector) {
+    if (this.player1turn) {
+      this.player1.moves.push(sector)
+    } else {
+      this.player2.moves.push(sector)
+    }
+    this.checkForWin()
+    this.passTurn()
   }
   resetBoard() {
 
   }
   passTurn() {
-
+    if (this.player1turn) {
+      this.player1turn = false
+    } else {
+      this.player1turn = true
+    }
   }
   checkForWin() {
 
